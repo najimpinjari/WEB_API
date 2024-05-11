c4b42ddb7e82e872046d5b5e1a13e394676a39e5
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-05-11 18:49:05 +0530

    Add files via upload

---
+++ Api_1/Controllers/EmployeeDataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_10.Controllers
+{
+    public class EmployeeDataController : ApiController
+    {
+        string[] employees = { "arbaj", "Akash", "Rupesh", "shailesh" };
+
+        [HttpGet]
+        public string[] GetEmployee()
+        {
+            return employees;
+        }
+
+        public string GetEmployeeById( int id )
+        {
+            return employees[id];
+        }
+    }
+}
