44dfb28e56e43383c1c2b535471b36b560fc3779
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-05-12 19:01:59 +0530

    Add files via upload

---
+++ ASP_MVC_WEB_API_CRUD/Controllers/CrudAPIController.cs
@@ -0,0 +1,32 @@
+﻿using ASP_MVC_WEB_API_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace ASP_MVC_WEB_API_CRUD.Controllers
+{
+    public class CrudAPIController : ApiController
+    {
+        WEB_API_crudDBEntities db = new WEB_API_crudDBEntities();
+
+        [HttpGet]
+        public IHttpActionResult GetEmployee()
+        {
+            List<Employee> List = db.Employees.ToList();
+            return Ok(List);
+        }
+
+        [HttpPost]
+        public IHttpActionResult EmpInsert( Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();   
+
+            return Ok();
+        }
+    }
+}
---
+++ ASP_MVC_WEB_API_CRUD/Controllers/CrudMVCController.cs
@@ -0,0 +1,54 @@
+﻿using ASP_MVC_WEB_API_CRUD.Models;
+using Newtonsoft.Json; // Make sure you have Newtonsoft.Json package installed via NuGet
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ASP_MVC_WEB_API_CRUD.Controllers
+{
+    public class CrudMVCController : Controller
+    {
+        HttpClient client = new HttpClient();
+
+        public async Task<ActionResult> Index()
+        {
+            List<Employee> emp_list = new List<Employee>();
+            client.BaseAddress = new Uri("http://localhost:52712/api/CrudAPI");
+
+            var response = await client.GetAsync("CrudAPI");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                emp_list = JsonConvert.DeserializeObject<List<Employee>>(content);
+            }
+
+            return View(emp_list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create( Employee emp)
+        {
+            client.BaseAddress = new Uri("http://localhost:52712/api/CrudAPI");
+
+            var response = client.PostAsJsonAsync<Employee>("CrudAPI",emp);
+            response.Wait();
+
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Create");
+        }
+    }
+}
