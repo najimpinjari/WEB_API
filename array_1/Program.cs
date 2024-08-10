611b573834cd5b24982d82bb6247ada65b018b37
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-08-10 21:38:42 +0530

    Add files via upload

---
+++ array_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please inter your target");
+            int target = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= target; i++) 
+            {
+                if (i % 2== 0 ) 
+                {
+                    Console.Write($" {i} ");  
+                }
+            }
+            Console.WriteLine();
+
+
+           Console.ReadLine();
+
+
+        }
+    }
+}
